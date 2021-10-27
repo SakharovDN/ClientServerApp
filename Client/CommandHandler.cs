fb7cc4ebab114e7ebc4b25309c9a9031e24c4fc7
SakharovDN <SakharovDN@ORPO-1203>
2021-10-27 11:40:04 +0700

    add button handling

--- Client/CommandHandler.cs
+++ Client/CommandHandler.cs
@@ -14,9 +14,6 @@
 
         #region Events
 
-        /// <summary>
-        /// Wires CanExecuteChanged event
-        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
@@ -27,11 +24,6 @@
 
         #region Constructors
 
-        /// <summary>
-        /// Creates instance of the command handler
-        /// </summary>
-        /// <param name = "action">Action to be executed by the command</param>
-        /// <param name = "canExecute">A bolean property to containing current permissions to execute the command</param>
         public CommandHandler(Action<object> action, Func<object, bool> canExecute = null)
         {
             _action = action;
@@ -42,11 +34,6 @@
 
         #region Methods
 
-        /// <summary>
-        /// Forcess checking if execute is allowed
-        /// </summary>
-        /// <param name = "parameter"></param>
-        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return _canExecute == null || _canExecute(parameter);
