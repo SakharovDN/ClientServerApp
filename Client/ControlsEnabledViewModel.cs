ec45d9b146d33e6dc50966e06df22ec0ea42dcab
SakharovDN <SakharovDN@ORPO-1203>
2021-11-02 14:39:32 +0700

    add button handling

---
+++ Client/ControlsEnabledViewModel.cs
@@ -0,0 +1,179 @@
+﻿namespace Client
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using Annotations;
+
+    public class ControlsEnabledViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private bool _addressIsEnabled;
+        private bool _portIsEnabled;
+        private bool _clientNameIsEnabled;
+        private bool _messageIsEnabled;
+        private bool _buttonStartIsEnabled;
+        private bool _buttonStopIsEnabled;
+        private bool _buttonSendIsEnabled;
+        private bool _buttonLoginIsEnabled;
+        private bool _buttonClearIsEnabled;
+
+        #endregion
+
+        #region Properties
+
+        public bool AddressIsEnabled
+        {
+            get => _addressIsEnabled;
+            set
+            {
+                _addressIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool PortIsEnabled
+        {
+            get => _portIsEnabled;
+            set
+            {
+                _portIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ClientNameIsEnabled
+        {
+            get => _clientNameIsEnabled;
+            set
+            {
+                _clientNameIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool MessageIsEnabled
+        {
+            get => _messageIsEnabled;
+            set
+            {
+                _messageIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ButtonStartIsEnabled
+        {
+            get => _buttonStartIsEnabled;
+            set
+            {
+                _buttonStartIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ButtonStopIsEnabled
+        {
+            get => _buttonStopIsEnabled;
+            set
+            {
+                _buttonStopIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ButtonSendIsEnabled
+        {
+            get => _buttonSendIsEnabled;
+            set
+            {
+                _buttonSendIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ButtonLoginIsEnabled
+        {
+            get => _buttonLoginIsEnabled;
+            set
+            {
+                _buttonLoginIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ButtonClearIsEnabled
+        {
+            get => _buttonClearIsEnabled;
+            set
+            {
+                _buttonClearIsEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Constructors
+
+        public ControlsEnabledViewModel()
+        {
+            SetDefaultControlsState();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void SetDefaultControlsState()
+        {
+            AddressIsEnabled = true;
+            PortIsEnabled = true;
+            MessageIsEnabled = false;
+            ClientNameIsEnabled = false;
+            ButtonStartIsEnabled = true;
+            ButtonStopIsEnabled = false;
+            ButtonLoginIsEnabled = false;
+            ButtonSendIsEnabled = false;
+        }
+
+        public void SetAfterStartControlsState()
+        {
+            AddressIsEnabled = false;
+            PortIsEnabled = false;
+            MessageIsEnabled = false;
+            ClientNameIsEnabled = true;
+            ButtonStartIsEnabled = false;
+            ButtonStopIsEnabled = true;
+            ButtonLoginIsEnabled = true;
+            ButtonSendIsEnabled = false;
+        }
+
+        public void SetAfterLoginControlsState()
+        {
+            AddressIsEnabled = false;
+            PortIsEnabled = false;
+            MessageIsEnabled = true;
+            ClientNameIsEnabled = false;
+            ButtonStartIsEnabled = false;
+            ButtonStopIsEnabled = true;
+            ButtonLoginIsEnabled = false;
+            ButtonSendIsEnabled = true;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,67 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Client
+﻿namespace Client
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructors
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new ViewModelBase();
-            SetDefaultButtonState();
         }
 
-        private void SetDefaultButtonState()
-        {
-            StartButton.IsEnabled = true;
-            StopButton.IsEnabled = false;
-            LoginButton.IsEnabled = false;
-            SendButton.IsEnabled = false;
-
-            AddressTextBox.IsEnabled = true;
-            PortTextBox.IsEnabled = true;
-            MessageTextBox.IsEnabled = false;
-            LoginTextBox.IsEnabled = false;
-        }
-
-        private void HandleButtonStartClick(object sender, EventArgs e)
-        {
-            try
-            {
-                //_currentTransport = TransportFactory.Create((TransportType)_transport.SelectedItem);
-                //_currentTransport.ConnectionStateChanged += HandleConnectionStateChanged;
-                //_currentTransport.MessageReceived += HandleMessageReceived;
-                //_currentTransport.Connect(_serverAddress.Text, _serverPort.Text);
-
-                AddressTextBox.IsEnabled = false;
-                PortTextBox.IsEnabled = false;
-                MessageTextBox.IsEnabled = false;
-                StartButton.IsEnabled = false;
-                StopButton.IsEnabled = true;
-                LoginButton.IsEnabled = true;
-                LoginTextBox.IsEnabled = true;
-            }
-            catch (Exception ex)
-            {
-                MessagesListBox.Items.Add(ex.Message);
-                SetDefaultButtonState();
-            }
-        }
+        #endregion
     }
 }
